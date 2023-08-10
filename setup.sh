379d137a37f936f1e18cf50c9e8d9826e3d82ab7
kaven <kireev.artur@gmail.com>
2023-08-10 19:24:58 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ sudo apt-get update
 sudo apt install wget git python3 python3-venv build-essential net-tools awscli -y
 
 # install CUDA (from https://developer.nvidia.com/cuda-downloads)
-wget https://developer.download.nvidia.com/compute/cuda/12.0.0/local_installers/cuda_12.0.0_525.60.13_linux.run
-sudo sh cuda_12.0.0_525.60.13_linux.run --silent
+wget https://developer.download.nvidia.com/compute/cuda/12.2.1/local_installers/cuda_12.2.1_535.86.10_linux.run
+sudo sh cuda_12.2.1_535.86.10_linux.run --silent
 
 # install git-lfs
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
@@ -13,17 +13,22 @@ sudo apt-get install git-lfs
 sudo -u ubuntu git lfs install --skip-smudge
 
 # download the SD model v2.1 and move it to the SD model directory
-sudo -u ubuntu git clone --depth 1 https://huggingface.co/stabilityai/stable-diffusion-2-1-base
-cd stable-diffusion-2-1-base/
-sudo -u ubuntu git lfs pull --include "v2-1_512-ema-pruned.ckpt"
+# sudo -u ubuntu git clone --depth 1 https://huggingface.co/stabilityai/stable-diffusion-2-1-base
+sudo -u ubuntu git clone --depth 1 https://huggingface.co/runwayml/stable-diffusion-v1-5/
+# cd stable-diffusion-2-1-base/
+cd stable-diffusion-v1-5/
+# sudo -u ubuntu git lfs pull --include "v2-1_512-ema-pruned.ckpt"
+sudo -u ubuntu git lfs pull --include "v1-5-pruned.ckpt"
 sudo -u ubuntu git lfs install --force
 cd ..
-mv stable-diffusion-2-1-base/v2-1_512-ema-pruned.ckpt stable-diffusion-webui/models/Stable-diffusion/
-rm -rf stable-diffusion-2-1-base/
+# mv stable-diffusion-2-1-base/v2-1_512-ema-pruned.ckpt stable-diffusion-webui/models/Stable-diffusion/
+# rm -rf stable-diffusion-2-1-base/
+mv stable-diffusion-v1-5/v1-5-pruned.ckpt stable-diffusion-webui/models/Stable-diffusion/
+mv stable-diffusion-v1-5/v1-inference.yaml stable-diffusion-webui/models/Stable-diffusion/v1-inference.yaml
 
 # download the corresponding config file and move it also to the model directory (make sure the name matches the model name)
-wget https://raw.githubusercontent.com/Stability-AI/stablediffusion/main/configs/stable-diffusion/v2-inference.yaml
-cp v2-inference.yaml stable-diffusion-webui/models/Stable-diffusion/v2-1_512-ema-pruned.yaml
+# wget https://raw.githubusercontent.com/Stability-AI/stablediffusion/main/configs/stable-diffusion/v2-inference.yaml
+# cp v2-inference.yaml stable-diffusion-webui/models/Stable-diffusion/v2-1_512-ema-pruned.yaml
 
 # change ownership of the web UI so that a regular user can start the server
 sudo chown -R ubuntu:ubuntu stable-diffusion-webui/
